86331d51ca314a09f41ced75f27d6b960a2e0f7a
Po <po.grammer.lin@gmail.com>
2017-12-08 16:13:33 -0600

    Completed Select Exercise

---
+++ select_exercise.sql
@@ -0,0 +1,13 @@
+use codeup_test_db;
+
+select name from albums where artist = 'Pink Floyd';
+
+select release_date from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+select genre from albums where name = 'Nevermind';
+
+select * from albums where release_date between 1990 and 1999;
+
+select * from albums where sales < 40000000;
+
+select * from albums where genre like '%rock%' OR genre like '%Rock%';
