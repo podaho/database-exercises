4555727659f897090e1bcfec335176870a120416
Po <po.grammer.lin@gmail.com>
2017-12-11 10:57:50 -0600

    Added some captions

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,12 +1,22 @@
 use codeup_test_db;
 
+select 'Albums Released Later than 1991' as '';
+
 select * from albums where release_date > 1991;
 
 delete from albums where release_date > 1991;
 
+
+
+select 'Albums with a Genre of Disco' as '';
+
 select * from albums where genre like '%disco%' or genre like '%Disco%';
 
 delete from albums where genre like '%disco%' or genre like '%Disco%';
+
+
+
+select 'Albums by Whitney Houston' as '';
 
 select * from albums where artist = 'Whitney Houston';
 
